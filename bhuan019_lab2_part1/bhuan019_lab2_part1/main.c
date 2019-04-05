6e89aaa05e7d59227a577afbd78f4f35aee2d204
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-04 17:08:24 -0700

    Update main.c

--- bhuan019_lab2_part1/bhuan019_lab2_part1/main.c
+++ bhuan019_lab2_part1/bhuan019_lab2_part1/main.c
@@ -9,26 +9,23 @@
 
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	while(1)
-	{
-		// 1) Read input
-		tmpA = PINA & 0x01;
-		// 2) Perform computation
-		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if (tmpA == 0x01) { // True if PA0 is 1
-			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
-			// (clear rightmost 2 bits, then set to 01)
+	DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as input
+	DDRB = 0xFF; PORTB = 0x00; //Configure port B's 8 pin as output
+	//initialized output on PORTB to 0x00
+	
+	unsigned char input1 = 0x00;
+	unsigned char input2 = 0x00;
+	
+	while(1){
+		//read input
+		input1 = PINA & 0x01;//PA0
+		input2 = PINA & 0x02;//PA1
+		if(input1 == 0x01 && input2 == 0x00){
+			PORTB = 0x01;//writing to registers
 		}
-		else {
-			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
-			// (clear rightmost 2 bits, then set to 10)
+		else{
+			PORTB = 0x01;
 		}
-		// 3) Write output
-		PORTB = tmpB;
 	}
 	return 0;
 }
