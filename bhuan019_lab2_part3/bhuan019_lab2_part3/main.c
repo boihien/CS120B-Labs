6572bb1dc622f746b83d4ac8017b5a6315852d4a
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-05 19:55:39 -0700

    first commit

---
+++ bhuan019_lab2_part3/bhuan019_lab2_part3/main.c
@@ -0,0 +1,39 @@
+/*
+ * bhuan019_lab2_part3.c
+ *
+ * Created: 05-Apr-19 19:46:33
+ * Author : Boi-Hien Huang
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	/* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF;//set Port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00;//set Port C's 8 pins as outputs
+	//PORTC initialized to 0
+	unsigned char space0 = 0x00;
+	unsigned char space1 = 0x00;
+	unsigned char space2 = 0x00;
+	unsigned char space3 = 0x00;
+	unsigned char cntavail = 0x00;
+	unsigned char full = 0x00;
+	while (1)
+	{
+		//read in input
+		space0 = PINA & 0x01;
+		space1 = PINA & 0x02;
+		space2 = PINA & 0x04;
+		space3 = PINA & 0x08;
+		
+		full = (space0 & 0x01) && (space1 & 0x02) && (space2 & 0x04) && (space3 & 0x08);
+		
+		cntavail = !(space0 & 0x01) + !((space1 & 0x02) >> 1) + !((space2 & 0x04) >> 2) + !((space3 & 0x08) >> 3);
+		
+		PORTC = cntavail | (full << 7);
+	}
+	return 0;
+}
+
