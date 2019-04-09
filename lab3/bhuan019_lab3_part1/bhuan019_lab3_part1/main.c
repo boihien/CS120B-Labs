f74cff6f1b9f7368c13992ce735d093c262a7561
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-09 15:54:32 -0700

    hi
    hi

---
+++ lab3/bhuan019_lab3_part1/bhuan019_lab3_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * bhuan019_lab3_part1.c
+ *
+ * Created: 09-Apr-19 15:53:31
+ * Author : Boi-Hien Huang
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
