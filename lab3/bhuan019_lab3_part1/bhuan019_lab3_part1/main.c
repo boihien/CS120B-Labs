432c9376ca7847c57b5da2bb1e3d6c4658fa2806
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-09 19:22:27 -0700

    hi

--- lab3/bhuan019_lab3_part1/bhuan019_lab3_part1/main.c
+++ lab3/bhuan019_lab3_part1/bhuan019_lab3_part1/main.c
@@ -2,7 +2,8 @@
  * bhuan019_lab3_part1.c
  *
  * Created: 09-Apr-19 15:53:31
- * Author : Boi-Hien Huang
+ * Author : Boi-Hien Huang 
+ * Partner : Tommy Ngo
  */ 
 
 #include <avr/io.h>
@@ -10,9 +11,24 @@
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as input
+	DDRB = 0x00; PORTB = 0xFF; //Configure port B's 8 pin as input
+	DDRC = 0xFF; PORTC = 0x00; //COnfigure port C's 8 pin as output
+	//initialized output on PORTB to 0x00
+	
+	unsigned char P0 = 0x00;
+	unsigned char P1 = 0x00;
+	unsigned char numA = 0x00;
+	unsigned char numB = 0x00;
+	
     while (1) 
     {
+		P0 = PINA & 0xFF;
+		P1 = PINB & 0xFF;
+		numA = (P0 & 0x01) + ((P0 & 0x02) >> 1) + ((P0 & 0x04) >> 2 ) + ((P0 & 0x08) >> 3) + ((P0 & 0x010) >> 4) + ((P0 & 0x20) >> 5) + ((P0 & 0x40) >> 6) + ((P0 & 0x80) >> 7);
+		numB = (P1 & 0x01) + ((P1 & 0x02) >> 1) + ((P1 & 0x04) >> 2 ) + ((P1 & 0x08) >> 3) + ((P1 & 0x010) >> 4) + ((P1 & 0x20) >> 5) + ((P1 & 0x40) >> 6) + ((P1 & 0x80) >> 7);
+		PORTC = numA + numB;
+		
     }
 }
 
---
+++ lab3/bhuan019_lab3_part2/bhuan019_lab3_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * bhuan019_lab3_part2.c
+ *
+ * Created: 09-Apr-19 19:22:11
+ * Author : Boi-Hien Huang
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
