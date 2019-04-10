dbda19ccb5393febdc14153feb740a57d70f66b5
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-10 00:42:31 -0700

    Update main.c

--- lab3/bhuan019_lab3_part2/bhuan019_lab3_part2/main.c
+++ lab3/bhuan019_lab3_part2/bhuan019_lab3_part2/main.c
@@ -3,6 +3,7 @@
  *
  * Created: 09-Apr-19 19:22:11
  * Author : Boi-Hien Huang
+ * Partner: Tommy Ngo
  */ 
 
 #include <avr/io.h>
@@ -10,9 +11,13 @@
 
 int main(void)
 {
+	DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as input
+	DDRB = 0xFF; PORTB = 0x00; //Configure port B's 8 pin as output
     /* Replace with your application code */
+	
     while (1) 
     {
+		
     }
 }
 
