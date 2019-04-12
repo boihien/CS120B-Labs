5914cfa80f3f9e46956875820b15ec842c1fe8a7
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-12 16:33:47 -0700

    first

--- lab4/bhuan019_lab4_part3/bhuan019_lab4_part3/main.c
+++ lab4/bhuan019_lab4_part3/bhuan019_lab4_part3/main.c
@@ -6,112 +6,89 @@
  */ 
 
 #include <avr/io.h>
+enum states {init, start, pass, wait, unlock, incorrect} state;
 
-enum States {init, start, hashtag, y, wait1, wait2, lock} state;
-
-void tick() {
-	switch (state) { // Transitions
+void lock() {
+	switch (state) {
 		case init:
+		state = start;
+		break;
+		
+		case start:
+		if (PINA & 0x04) {
+			state = wait;
+		}
+		break;
+		
+		case wait:
+		if (!(PINA & 0x04)) {
+			state = pass;
+		}
+		break;
+		
+		case pass:
+		if (PINA & 0x02) {
+			state = unlock;
+		}
+		else  if (!PINA){
+			state = pass;
+		}
+		else {
+			state = incorrect;
+		}
+		break;
+		
+		case unlock:
+		if (PINA & 0x80) {
 			state = start;
-			break;
+		}
+		break;
+		
+		case incorrect:
+		if (!PINA) {
+			state = start;
+		}
+		break;
+	}
+	switch (state) {
+		case init:
+		PORTC = state;
+		break;
+		
 		case start:
-			if((PINA & 0x04) == 0x04){
-				state = wait1;
-				break;
-			}
-			else if((PINA & 0x80) == 0x80){
-				state = lock;
-				break;
-			}
-			else{
-				state = start;
-				break;
-			}
-		case wait1:
-			if((PINA & 0x04) == 0x00){
-				state = hashtag;
-				break;
-			}
-			else if((PINA & 0x80) == 0x80){
-				state = lock;
-				break;
-			}
-			else{
-				state = start;
-				break;
-			}
-			
-		case hashtag:
-			if((PINA & 0x02) == 0x02){
-				state = wait2;
-				break;
-			}
-			else if((PINA & 0x80) == 0x80){
-				state = lock;
-				break;
-			}
-			else{
-				state = start;
-				break;
-			}
-		case wait2:
-			if((PINA & 0x02) == 0x00){
-				state = y;
-				break;
-			}
-			else if((PINA & 0x80) == 0x80){
-				state = lock;
-				break;
-			}
-			else{
-				state = start;
-				break;
-			}
-		case y:
-			if((PINA & 0x80) == 0x80){
-				state = lock;
-				break;
-			}
-			
-		default:
-			break;
-	} // Transitions
-
-	switch(state) { // Actions
-		case init: 
-			PORTB = 0x00;
-			PORTC = 0x00;
-			break;
-		case start:
-			PORTC = 0x01;
-			break;
-		case hashtag:
-			PORTC = 0x02;
-			break;
-		case wait1:
-			PORTC = 0x03;
-			break;
-		case y:
-			PORTB = 0x01;
-			PORTC = 0x04;
-			break;
-		case wait2:
-			PORTC = 0x05;
-			break;
-		case lock:
-			PORTB = 0x00;
-			PORTC = 0x06;
-			break;
-			
-	} // Actions
+		PORTC = state;
+		PORTB = PORTB & 0xFE;
+		break;
+		
+		case pass:
+		PORTC = state;
+		break;
+		
+		case wait:
+		PORTC = state;
+		break;
+		
+		case incorrect:
+		PORTC = state;
+		break;
+		
+		case unlock:
+		PORTC = state;
+		PORTB = PORTB | 0x01;
+		break;
+		
+	}
 }
 
-int main(void) {
+int main(void)
+{
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xFF; PORTC = 0x00;
-	state = init; // Initial state
-	while(1) { tick(); }
+	DDRC = 0x00; PORTC = 0xFF;
+	/* Replace with your application code */
+	state = init;
+	while (1)
+	{
+		lock();
+	}
 }
-
-
