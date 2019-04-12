499607c3484dfab2f174bcbc26ba727bf4da97be
Boi-Hien Huang <bhuan019@ucr.edu>
2019-04-11 22:02:52 -0700

    first

--- lab4/bhuan019_lab4_part2/bhuan019_lab4_part2/main.c
+++ lab4/bhuan019_lab4_part2/bhuan019_lab4_part2/main.c
@@ -8,11 +8,107 @@
 #include <avr/io.h>
 
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+enum States {start, init, inc, dec, wait_inc, wait_dec, reset} state;
+
+void tick() {
+	switch (state) { // Transitions
+		case init:
+			state = start;
+			break;
+		
+		case start:
+			if((PINA & 0x01) == 0x01){
+				state = wait_inc;
+				break;
+			}
+			else{
+				state = start;
+				break;
+			}
+			if((PINA & 0x02) == 0x01){
+				state = wait_dec;
+				break;
+			}
+			else{
+				state = start;
+				break;
+			}
+		
+		case wait_inc:
+			if((PINA & 0x01) == 0x00){
+				state = inc;
+				break;
+			}
+			if((PINA & 0x02) == 0x01){
+				state = reset;
+				break;
+			}
+			
+		case inc:
+			state = start;
+			break;
+			
+		case wait_dec:
+			if((PINA & 0x02) == 0x00){
+				state = dec;
+				break;
+			}
+			if((PINA & 0x01) == 0x01){
+				state = reset;
+				break;
+			}
+			
+		case dec:
+			state = start;
+			break;
+			
+		case reset:
+			state = start;
+			break;
+
+	} // Transitions
+
+	switch(state) { // Actions
+		case init:
+			PORTC = 0x07;
+			break;
+			
+		case start:
+			break;
+		
+		case inc:
+			if(PORTC < 0x09){
+				PORTC = PORTC + 0x01;
+				break;
+			}
+		
+		case dec:
+			if(PORTC > 0x00){
+				PORTC = PORTC - 0x01;
+				break;
+			}
+			
+		case wait_dec:
+			break;
+			
+		case wait_inc:
+			break;
+			
+		case reset:
+			PORTC = 0x00;
+			break;
+			
+		default:
+			break;
+		
+		
+	} // Actions
 }
 
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x07;
+	
+	state = init;
+	while(1) { tick(); }
+}
