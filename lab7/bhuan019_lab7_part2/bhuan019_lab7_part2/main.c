002f0fbb152d20c31b685661bf68144268f1c34a
Boi-Hien Huang <bhuan019@ucr.edu>
2019-05-02 16:28:32 -0700

    fixed lab7

--- lab7/bhuan019_lab7_part2/bhuan019_lab7_part2/main.c
+++ lab7/bhuan019_lab7_part2/bhuan019_lab7_part2/main.c
@@ -5,455 +5,170 @@
  * Author : Boi-Hien Huang
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "io.c"
 
-unsigned char i = 0x00;
-unsigned char c = '0';
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
 
-// TimerISR() sets this to 1. C programmer should clear to 0.
-volatile unsigned char TimerFlag = 0;
-
-//Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
 unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
-unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1 ms ticks
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
 
-void TimerOn()
-{
+void TimerOn() {
 	// AVR timer/counter controller register TCCR1
-	// bit3 = 0; CTC mode (clear timer on compare)
-	// bit2bit1bit0 = 011: prescaler /64
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
 	// 00001011: 0x0B
-	// SO, 8 MHz clock or 8,000,000 / 64 = 125,000 ticks/s
-	// THUS, TCNT! register will count at 125,000 ticks/s
-	TCCR1B = 0x0B;
-	
-	//AVR output compare register OCR1A.
-	// Timer interrupt will be generated when TCNT! == OCR1A
-	// We want a 1 ms tick. 0.001S * 125,000 ticks/s = 125
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
 	// So when TCNT1 register equals 125,
-	// ! ms has passed. Thus, we compare to 125.
-	OCR1A = 125;
+	// 1 ms has passed. Thus, we compare to 125.
 	// AVR timer interrupt mask register
-	// bit1: OCIE1A -- enables compare match interrupt
-	TIMSK1 = 0x02;
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
 	//Initialize avr counter
 	TCNT1=0;
-	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
 	_avr_timer_cntcurr = _avr_timer_M;
-	//Enable global interrupts: 0x80: 1000000
-	SREG |= 0x80;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
 }
-void TimerOff()
-{
-	// bit3bit1bit0=000: timer off
-	TCCR1B = 0x00;
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
 }
-void TimerISR()
-{
+
+void TimerISR() {
 	TimerFlag = 1;
 }
+
 // In our approach, the C programmer does not touch this ISR, but rather TimerISR()
-ISR(TIMER1_COMPA_vect)
-{
-	// CPU automatically calls when TCNT1 == OCR1
-	// (every 1 ms per TimerOn settings)
-	// Count down to 0 rather than up to TOP (results in a more efficient comparison)
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0)
-	{
-		// Call the ISR that the user uses
-		TimerISR();
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
 		_avr_timer_cntcurr = _avr_timer_M;
 	}
 }
+
 // Set TimerISR() to tick every M ms
-void TimerSet(unsigned long M)
-{
+void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
 	_avr_timer_cntcurr = _avr_timer_M;
 }
-void Tick();
-enum States {Start, INIT, INC, DEC, WAIT, RESET}state;
+enum States {init, start, inc, dec, wait_inc, wait_dec, reset} state;
+unsigned char counter = 0x00;
 
-
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00; //LCD data lines
-	DDRD = 0xFF; PORTD = 0x00; //LCD control lines
-	
-	//Initializes the LCD display
-	LCD_init();
-	
-	TimerSet(125);
-	TimerOn();
-	
-	while(1)
-	{
-		Tick();
-		while (!TimerFlag);
-		TimerFlag = 0;
-	}
-}
-
-void Tick()
-{
-	switch(state) // transitions
-	{
-		case Start:
-		{
-			LCD_Cursor(1);
-			LCD_WriteData('0');
-			state = INIT;
-			break;
+void tick(){
+	switch(state){//transitions
+		case init:
+		if (~PINA & 0x01) {
+			state = wait_inc;
 		}
+		else if (~PINA & 0x02) {
+			state = wait_dec;
+		}
+		break;
 		
-		case INIT:
-		{
-			if((~PINA & 0x03) == 0x01)
-			{
-				state = INC; break;
-			}
-			else if((~PINA & 0x03) == 0x02)
-			{
-				state = DEC; break;
-			}
-			else if((~PINA & 0x03) == 0x03)
-			{
-				state = RESET; break;
-			}
-			else
-			{
-				state = INIT; break;
-			}
+		case start:
+		if (~PINA & 0x01) {
+			state = wait_inc;
 		}
+		else if (~PINA & 0x02) {
+			state = wait_dec;
+		}
+		break;
 		
-		case INC:
-		{
-			state = WAIT; break;
-		}
+		case wait_inc:
+		state = inc;
+		break;
 		
-		case DEC:
-		{
-			state = WAIT; break;
+		case inc:
+		if (!(~PINA & 0x01)) {
+			state = start;
 		}
+		else if (~PINA & 0x02) {
+			state = reset;
+		}
+		break;
 		
-		case WAIT:
-		{
-			if((~PINA & 0x03) == 0x01)
-			{
-				state = INC; break;
-			}
-			else if((~PINA & 0x03) == 0x02)
-			{
-				state = DEC; break;
-			}
-			else if((~PINA & 0x03) == 0x03)
-			{
-				state = RESET; break;
-			}
-			else if((~PINA & 0x03) == 0x00)
-			{
-				state = INIT; break;
-			}
-			else
-			{
-				state = WAIT; break;
-			}
-		}
+		case wait_dec:
+		state = dec;
+		break;
 		
-		case RESET:
-		{
-			if((~PINA & 0x03) == 0x00)
-			{
-				state = INIT; break;
-			}
-			else
-			{
-				state = RESET; break;
-			}
+		case dec:
+		if (!(~PINA & 0x02)) {
+			state = start;
 		}
+		else if (~PINA & 0x01) {
+			state = reset;
+		}
+		break;
+		
+		case reset:
+		state = start;
+		break;
 		
 		default:
 		break;
+		
+		
 	}
-	
-	switch(state) //state actions
-	{
-		case Start:
+	switch (state){
+		case start:
+		LCD_Cursor(1);
+		LCD_WriteData('0');
 		break;
-		
-		case INIT:
+		case init:
 		break;
-		
-		case INC:
-		{
-			if(i >= 9)
-			{
-				i = 9;
-			}
-			else
-			{
-				++i;
-			}
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;
-		}
-		
-		case DEC:
-		{
-			if(i <= 0)
-			{
-				i = 0;
-			}
-			else
-			{
-				--i;
-			}
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;
-		}
-		
-		case WAIT:
+		case inc:
 		break;
-		
-		case RESET:
-		{
-			i = 0;
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;
+		case dec:
+		break;
+		case wait_dec:
+		if(counter <= 0){
+			counter = 0;
 		}
-	}
-}/*
- * bhuan019_lab7_part1.c
- *
- * Created: 25-Apr-19 19:40:38
- * Author : Boi-Hien Huang
- */
-#include <avr/io.h>
-#include "io.c"
-
-unsigned char i = 0x00;
-unsigned char c = '0';
-
-// TimerISR() sets this to 1. C programmer should clear to 0.
-volatile unsigned char TimerFlag = 0;
-
-//Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
-unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
-unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1 ms ticks
-
-void TimerOn()
-{
-	// AVR timer/counter controller register TCCR1
-	// bit3 = 0; CTC mode (clear timer on compare)
-	// bit2bit1bit0 = 011: prescaler /64
-	// 00001011: 0x0B
-	// SO, 8 MHz clock or 8,000,000 / 64 = 125,000 ticks/s
-	// THUS, TCNT! register will count at 125,000 ticks/s
-	TCCR1B = 0x0B;
-	
-	//AVR output compare register OCR1A.
-	// Timer interrupt will be generated when TCNT! == OCR1A
-	// We want a 1 ms tick. 0.001S * 125,000 ticks/s = 125
-	// So when TCNT1 register equals 125,
-	// ! ms has passed. Thus, we compare to 125.
-	OCR1A = 125;
-	// AVR timer interrupt mask register
-	// bit1: OCIE1A -- enables compare match interrupt
-	TIMSK1 = 0x02;
-	//Initialize avr counter
-	TCNT1=0;
-	// TimerISR will be called every _avr_timer_cntcurr milliseconds
-	_avr_timer_cntcurr = _avr_timer_M;
-	//Enable global interrupts: 0x80: 1000000
-	SREG |= 0x80;
-}
-void TimerOff()
-{
-	// bit3bit1bit0=000: timer off
-	TCCR1B = 0x00;
-}
-void TimerISR()
-{
-	TimerFlag = 1;
-}
-// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
-ISR(TIMER1_COMPA_vect)
-{
-	// CPU automatically calls when TCNT1 == OCR1
-	// (every 1 ms per TimerOn settings)
-	// Count down to 0 rather than up to TOP (results in a more efficient comparison)
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0)
-	{
-		// Call the ISR that the user uses
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
+		else{
+			counter--;
+		}
+		LCD_Cursor(1);
+		LCD_WriteData(counter + '0');
+		break;
+		case wait_inc:
+		if(counter >= 9){
+			counter = 9;
+		}
+		else{
+			counter++;
+		}
+		LCD_Cursor(1);
+		LCD_WriteData(counter + '0');
+		break;
+		case reset:
+		counter = 0;
+		LCD_Cursor(1);
+		LCD_WriteData(counter + '0');
+		break;
 	}
 }
-// Set TimerISR() to tick every M ms
-void TimerSet(unsigned long M)
-{
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-void Tick();
-enum States {Start, INIT, INC, DEC, WAIT, RESET}state;
-
-
-int main(void)
-{
+int main(void){
+	state = start;
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00; //LCD data lines
-	DDRD = 0xFF; PORTD = 0x00; //LCD control lines
-	
-	//Initializes the LCD display
-	LCD_init();
-	
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 	TimerSet(125);
 	TimerOn();
-	
-	while(1)
-	{
-		Tick();
-		while (!TimerFlag);
+	while(1){
+		tick();
+		while(!TimerFlag);
 		TimerFlag = 0;
-	}
-}
-
-void Tick()
-{
-	switch(state) // transitions
-	{
-		case Start:
-		{
-			LCD_Cursor(1);
-			LCD_WriteData('0');
-			state = INIT;
-			break;
-		}
-		
-		case INIT:
-		{
-			if((~PINA & 0x03) == 0x01)
-			{
-				state = INC; break;
-			}
-			else if((~PINA & 0x03) == 0x02)
-			{
-				state = DEC; break;
-			}
-			else if((~PINA & 0x03) == 0x03)
-			{
-				state = RESET; break;
-			}
-			else
-			{
-				state = INIT; break;
-			}
-		}
-		
-		case INC:
-		{
-			state = WAIT; break;
-		}
-		
-		case DEC:
-		{
-			state = WAIT; break;
-		}
-		
-		case WAIT:
-		{
-			if((~PINA & 0x03) == 0x01)
-			{
-				state = INC; break;
-			}
-			else if((~PINA & 0x03) == 0x02)
-			{
-				state = DEC; break;
-			}
-			else if((~PINA & 0x03) == 0x03)
-			{
-				state = RESET; break;
-			}
-			else if((~PINA & 0x03) == 0x00)
-			{
-				state = INIT; break;
-			}
-			else
-			{
-				state = WAIT; break;
-			}
-		}
-		
-		case RESET:
-		{
-			if((~PINA & 0x03) == 0x00)
-			{
-				state = INIT; break;
-			}
-			else
-			{
-				state = RESET; break;
-			}
-		}
-		
-		default:
-		break;
-	}
-	
-	switch(state) //state actions
-	{
-		case Start:
-		break;
-		
-		case INIT:
-		break;
-		
-		case INC:
-		{
-			if(i >= 9)
-			{
-				i = 9;
-			}
-			else
-			{
-				++i;
-			}
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;
-		}
-		
-		case DEC:
-		{
-			if(i <= 0)
-			{
-				i = 0;
-			}
-			else
-			{
-				--i;
-			}
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;
-		}
-		
-		case WAIT:
-		break;
-		
-		case RESET:
-		{
-			i = 0;
-			LCD_Cursor(1);
-			LCD_WriteData(i + '0');
-			break;
-		}
 	}
 }
