6929310f4252366434d382d88887487bc43bd995
Boi-Hien Huang <bhuan019@ucr.edu>
2019-05-05 20:46:18 -0700

    part3

--- lab9/bhuan019_lab9_part3/bhuan019_lab9_part3/main.c
+++ lab9/bhuan019_lab9_part3/bhuan019_lab9_part3/main.c
@@ -1,9 +1,10 @@
 /*
- * bhuan019_lab9_part2.c
+ * bhuan019_lab9_part3.c
  *
- * Created: 05-May-19 16:06:18
+ * Created: 05-May-19 20:45:47
  * Author : Boi-Hien Huang
  */ 
+
 #include <avr/io.h>
 // 0.954 hz is lowest frequency possible with this function,
 // based on settings in PWM_on()
@@ -45,106 +46,107 @@ void PWM_off() {
 	TCCR0B = 0x00;
 }
 enum States{init, off, on, buttonON, increase, wait1, decrease, wait2}state;
-unsigned char counter = 0x00;
+
 void tick(){
 	const double scale[8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+	unsigned char counter = 0;
 	unsigned char temp = ~PINA & 0x07;
 	switch(state){//transitions
 		case init:
-			if(temp == 0x01){
-				state = on;
-			}
-			else{
-				state = init;
-			}
-			break;
+		if(temp == 0x01){
+			state = on;
+		}
+		else{
+			state = init;
+		}
+		break;
 		case off:
-			if(!(temp == 0x01)){
-				state = init;
-			}
-			else{
-				state = off;
-			}
-			break;
+		if(!(temp == 0x01)){
+			state = init;
+		}
+		else{
+			state = off;
+		}
+		break;
 		case on:
-			if(temp == 0x01){
-				state = on;
-			}
-			else{
-				state = buttonON;
-			}
-			break;
+		if(temp == 0x01){
+			state = on;
+		}
+		else{
+			state = buttonON;
+		}
+		break;
 		case buttonON:
-			if(temp == 0x02){
-				if(counter < 7){
-					counter = counter + 1;
-				}
-				state = increase;
+		if(temp == 0x02){
+			if(counter < 7){
+				counter++;
 			}
-			else if(temp == 0x04){
-				if(counter > 0){
-					counter = counter - 1;
-				}
-				state = decrease;
+			state = increase;
+		}
+		else if(temp == 0x04){
+			if(counter > 0){
+				counter--;
 			}
-			else if(temp == 0x01){
-				state = off;
-			}
-			else{
-				state = buttonON;
-			}
-			break;
+			state = decrease;
+		}
+		else if(temp == 0x01){
+			state = off;
+		}
+		else{
+			state = buttonON;
+		}
+		break;
 		case increase:
-			state = wait1;
-			break;
+		state = wait1;
+		break;
 		case wait1:
-			if(temp == 0x02){
-				state = wait1;
-			}
-			else{
-				state = buttonON;
-			}
-			break;
+		if(temp == 0x02){
+			state = wait1;
+		}
+		else{
+			state = buttonON;
+		}
+		break;
 		case decrease:
-			state = wait2;
-			break;
+		state = wait2;
+		break;
 		case wait2:
-			if(temp == 0x04){
-				state = wait2;
-			}
-			else{
-				state = buttonON;
-			}
-			break;
+		if(temp == 0x04){
+			state = wait2;
+		}
+		else{
+			state = buttonON;
+		}
+		break;
 	}
 	switch(state){//actions
 		case init:
-			PWM_off();
-			break;
+		PWM_off();
+		break;
 		case off:
-			PORTB = 0x01;
-			break;
+		PORTB = 0x01;
+		break;
 		case on:
-			PORTB = 0x02;
-			PWM_on();
-			break;
+		PORTB = 0x02;
+		PWM_on();
+		break;
 		case buttonON:
-			PORTB = 0x03;
-			break;
+		PORTB = 0x03;
+		break;
 		case increase:
-			PORTB = 0x04;
-			set_PWM(scale[counter]);
-			break;
+		PORTB = 0x04;
+		set_PWM(scale[counter]);
+		break;
 		case wait1:
-			PORTB = 0x05;
-			break;
+		PORTB = 0x05;
+		break;
 		case decrease:
-			PORTB = 0x06;
-			set_PWM(scale[counter]);
-			break;
+		PORTB = 0x06;
+		set_PWM(scale[counter]);
+		break;
 		case wait2:
-			PORTB = 0x07;
-			break;
+		PORTB = 0x07;
+		break;
 	}
 }
 
@@ -157,3 +159,4 @@ int main(void){
 		tick();
 	}
 }
+
