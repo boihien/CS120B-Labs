f920f441811b2ca4462661394d84e9902a880d25
Boi-Hien Huang <bhuan019@ucr.edu>
2019-05-05 21:13:16 -0700

    update lab8

---
+++ lab8/bhuan019_lab8_part2/bhuan019_lab8_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * bhuan019_lab8_part2.c
+ *
+ * Created: 05-May-19 20:54:52
+ * Author : Boi-Hien Huang
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ lab8/bhuan019_lab8_part3/bhuan019_lab8_part3/main.c
@@ -0,0 +1,18 @@
+/*
+ * bhuan019_lab8_part3.c
+ *
+ * Created: 05-May-19 20:55:24
+ * Author : Boi-Hien Huang
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
